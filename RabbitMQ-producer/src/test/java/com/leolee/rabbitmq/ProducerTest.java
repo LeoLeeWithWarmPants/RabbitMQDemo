b53865167232040bebe1a5180e323c717a20cc09
LeoLee <512240816@qq.com>
2020-11-07 16:26:49 +0800

    comfirm模式

--- RabbitMQ-producer/src/test/java/com/leolee/rabbitmq/ProducerTest.java
+++ RabbitMQ-producer/src/test/java/com/leolee/rabbitmq/ProducerTest.java
@@ -3,9 +3,11 @@ package com.leolee.rabbitmq;
 import com.leolee.rabbitmq.config.RabbitMQConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.lang.Nullable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -27,4 +29,39 @@ public class ProducerTest {
 
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "boot.test", "test msg send");
     }
+
+    /*
+     * 功能描述: <br>
+     * 〈确认模式〉
+     *  1.开启确认模式publisher-confirms: true
+     *  2.在RabbitTemplate定义ConfirmCallBack回调函数
+     * @Param: []
+     * @Return: void
+     * @Author: LeoLee
+     * @Date: 2020/11/7 15:58
+     */
+    @Test
+    public void testConfirmModel() {
+
+        //定义callback
+        /*
+         * correlationData 发送消息的时候的相关配置
+         * ack exchange是否成功收到消息，true成功
+         * cause 失败原因
+         */
+        rabbitTemplate.setConfirmCallback((@Nullable CorrelationData correlationData, boolean ack, @Nullable String cause) -> {
+            System.out.println("confirmCallBack executed");
+
+            if (ack) {
+                System.out.println("exchange 接收消息成功");
+            } else {
+                System.out.println("exchange 接收消息失败");
+                System.out.println("失败原因:" + cause);
+            }
+        });
+
+        //发送消息
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "boot.test", "test msg send");
+    }
+
 }
