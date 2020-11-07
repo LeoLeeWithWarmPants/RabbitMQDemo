a08c97448181554aa89fd3f2b9192757724ddb85
LeoLee <512240816@qq.com>
2020-11-07 20:27:07 +0800

    两种通过注解接收消息的方式

--- RabbitMQ-consumer/src/main/java/com/leolee/rabbitmq/MsgListener/TestMsgListener.java
+++ RabbitMQ-consumer/src/main/java/com/leolee/rabbitmq/MsgListener/TestMsgListener.java
@@ -1,6 +1,8 @@
 package com.leolee.rabbitmq.MsgListener;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
  * @Version V1.0
  **/
 @Component
+@RabbitListener(queues = "boot_queue")
 public class TestMsgListener {
 
     public static final String QUEUE_NAME = "boot_queue";
@@ -19,6 +22,23 @@ public class TestMsgListener {
     @RabbitListener(queues = "boot_queue")
     public void listenerQueue(Message message) {
 
-        System.out.println(message);
+        System.out.println("方法上@RabbitListener:" + new String(message.getBody()));
+    }
+
+
+    /*
+     * 功能描述: <br>
+     * 〈类上@RabbitListener + 方法上@RabbitHandler = 方法上@RabbitListener〉
+     * @Param: [str, channel, message]
+     * @Return: void
+     * @Author: LeoLee
+     * @Date: 2020/11/7 20:23
+     */
+    @RabbitHandler
+    public void listenerQueue(String str, Channel channel, Message message) {
+
+        System.out.println("类上@RabbitListener + 方法上@RabbitHandler:");
+        System.out.println("str:" + str);
+        System.out.println("msg:" + new String(message.getBody()));
     }
 }
